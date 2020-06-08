9ebe0ebc6845601fc76492ad9ca37222c0b0ebfc
lixinyan <380239907@qq.com>
2020-06-08 21:39:04 +0800

    JS跳转UI自动化

---
+++ utils/ConfigInfo.py
@@ -0,0 +1,34 @@
+
+
+URL = [
+       "kttp://galaxy.test.gifshow.com/yitian/activity/halloween.html#/",
+       ]
+
+baseConfig = {
+    'case1': {
+        "from": "yitianH5",
+        "data": {
+            "url": "test",
+            "jumpUrl": "kttp://galaxy.test.gifshow.com/yitian/activity/halloween.html#/"
+        }
+    },
+
+    'case2': {
+        "from": "yitianH5",
+        "data": {
+            "url": "test",
+            "jumpUrl": "kttp://galaxy.test.gifshow.com/yitian/activity/halloween.html#/"
+        }
+    },
+
+    'case3': {
+        "from": "yitianH5",
+        "data": {
+            "crid": "",
+            "jumpUrl": "m2u://home_cartoon",
+            "url": "https://h5.getkwai.com/yitian/cartoon-share",
+            "product": "WEB_YITIAN"
+        }
+    },
+
+}
